babe8b7e245fcf62d6e38d0fe3c1e431539a22be
VawnWuyest\Vawn_Wuyest <nvanquyet810@gmail.com>
2024-09-18 00:51:59 +0700

    refactor base data equiqment

--- Assets/_GAME/Scripts/Data/Items/Stat/DefineStat.cs
+++ Assets/_GAME/Scripts/Data/Items/Stat/DefineStat.cs
@@ -9,18 +9,31 @@ namespace VawnWuyest.Data
         FixedValue, 
         Percentage  
     }
+
+    [System.Serializable]
+    public enum StatDefine
+    {
+        Damage,
+        Hp,
+        AttackSpeed,
+        Dodge,
+        CritRate,
+        Armor,
+        Range,
+        MoveSpeed,
+        Luck,
+        HpRegen,
+        DamageToHp,
+        DamageToArmor
+    }
     [System.Serializable]
     public struct Stat{
-        [SerializeField] private float value;
-        public float Value => value;
+        [SerializeField] private StatDefine statDefine;
+        public StatDefine StatDefine => statDefine;
         [SerializeField] private StatType statType;
         public StatType StatType => statType;
-
-        public Stat(float valueStat = 0f, StatType statType = StatType.FixedValue)
-        {
-            this.value = valueStat;
-            this.statType = statType;
-        }
+        [SerializeField] private float value;
+        public float Value => value;
 
         public void SetValue(float value){
             this.value = value;
@@ -44,29 +57,10 @@ namespace VawnWuyest.Data
 
 #region Class Equiqment
 [System.Serializable]
-public struct EquiqmentStat{
-    public Stat damage;
-    public Stat hp;
-    public Stat attackSpeed;
-    public Stat dodge;
-    public Stat critRate;
-    public Stat armor;
-    public Stat range;
-    public Stat moveSpeed;
-    public Stat luck;
-    
-    // New stats
-    public Stat hpRegen;
-    public Stat damageToHp;
-    public Stat damageToArmor;  
-}
-
-
-[System.Serializable]
 public struct Equiqment
 {
     public string itemName;
-    public EquiqmentStat stats;
+    public Stat[] stats;
 }
 
 #endregion
