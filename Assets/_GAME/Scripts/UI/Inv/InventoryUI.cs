69db5cd46d2867b8b817782620a1cd8e7f7e67eb
fl2k3xb <fl2k3xb@gmail.com>
2024-12-05 03:48:02 +0700

    v1

--- Assets/_GAME/Scripts/UI/Inv/InventoryUI.cs
+++ Assets/_GAME/Scripts/UI/Inv/InventoryUI.cs
@@ -13,6 +13,18 @@ public class InventoryUI : Frame
     [SerializeField] private InventoryItemUI inventoryItemUI;
     [SerializeField] private int maxItem = 5;
 
+    [SerializeField] private ItemData itemdata1;
+    [SerializeField] private ItemData itemData2;
+    [SerializeField] private ItemData weaponData1;
+    [SerializeField] private ItemData weaponData2;
+
+    [SerializeField] private Player player;
+
+
+
+
+
+
     public Action OnButtonTetrisInvClickAction;
     public Action<ItemAttributeData> OnItemClickedAction;
 
@@ -31,13 +43,23 @@ public class InventoryUI : Frame
         }
         //Random Item Here
         var allItems = GameData.Instance.ItemData.GetAllValue();
+        var itemLv1 = itemdata1.GetAllValue();
+        var itemLv2 = itemData2.GetAllValue();
+        var weaponLv1 = weaponData1.GetAllValue();
+        var weaponLv2 = weaponData2.GetAllValue();
+
+        //var item1 = currentStat.GetStat(TypeStat.Luck);
+        //Debug.Log(item1);
+        
+       //ebug.Log(playerStat.CurrentStat.Stats.GetValue(0));
         for (int i = 0; i < maxItem; i++)
         {
             var item = Instantiate(inventoryItemUI, placeHolder);
-            item.Initialized(allItems[UnityEngine.Random.Range(0, allItems.Length - 1)], OnItemClickedAction);
+            item.Initialized(itemLv1[UnityEngine.Random.Range(0, itemLv1.Length - 1)], OnItemClickedAction);
             item.gameObject.SetActive(true);
         }
     }
 
     private void OnButtonTetrisInvClick() => OnButtonTetrisInvClickAction?.Invoke();
+
 }
